d09abe22607980faad125eb08e9de41658b41daa
Muchiri Mwangi <muchirimwangi09@gmail.com>
2025-07-11 22:27:52 +0300

    feat: Implement scroll-to-top functionality on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Pricing from './pages/Pricing'
@@ -7,6 +7,12 @@ import ContactUs from './pages/ContactUs'
 import Services from './pages/Services'
 
 const App = () => {
+  const location = useLocation()
+
+   useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [location]);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
